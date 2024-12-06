b4b196878da30458d6e7128a74e0633d8f6a5aa7
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2024-12-06 15:59:15 +0530

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,81 @@
 var express = require("express");
 var router = express.Router();
 let contactModel = require("./users");
+var passwordValidator = require('password-validator');
+
+var schema = new passwordValidator();
+
+// Add properties to it
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(8)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these  
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("contact");
+});
+router.get("/contactlog", function (req, res) {
+  res.render("contactlog");
 });
 router.get("/contact", function (req, res) {
   res.render("contact");
 });
-router.get("/about", function (req, res) {
-  res.render("about");
-});
-router.get("/create", async function (req, res) {
-  const createStudent = await studentModel.create({
-    name: "shubhanshi",
-    age: 21,
-    grade: "A",
-    address: "Bengali",
-  });
-  res.send(createStudent);
-});
-router.get("/getdata", async function (req, res) {
-  const studentData = await studentModel.find();
-  res.send(studentData);
-});
-router.get("/delete", async function (req, res) {
-  const deletedStudent = await studentModel.deleteMany();
-  res.send(deletedStudent);
-});
 
-router.get("/generate", async function (req, res) {
-  await studentModel.deleteMany();
-  let namesData = [
-    "Ramesh",
-    "Shubham",
-    "Akhilesh",
-    "Maaz",
-    "Hemant",
-    "bhawani",
-  ];
-  let addressData = [
-    "Bhopal",
-    "Sikar",
-    "Jaipur",
-    "Jodhpur",
-    "Kanpur",
-    "Varanasi",
-  ];
-  let gradeData = ["A", "B", "C", "D", "E", "F"];
-  let ageData = [21, 22, 23, 24, 25, 26];
+/* contact page */
 
-  for (let i = 0; i < 10; i++) {
-    let randomIndex = Math.floor(Math.random() * 5) + 1;
-
-    studentModel.create({
-      name: namesData[randomIndex],
-      address: addressData[randomIndex],
-      age: ageData[randomIndex],
-      grade: gradeData[randomIndex],
-    });
-  }
-  res.send("data is Created");
-});
-
-// Contact Form Message Storage
-router.post("/create-request", async (req, res) => {
+router.post("/create", async (req, res) => {
   try {
-    await contactModel.create({
-      name: req.body.name,
-      email: req.body.email,
-      message: req.body.message,
-      contact: req.body.number,
-    });
-  } catch (error) {
-    console.log("Some Error Occured");
-  }
-  res.send("Data Submitted");
+    let user = await contactModel.find({ name: req.body.name });
+    const passwordValidation = schema.validate(req.body.password);
+    let invalid = "Password does not meet the required criteria. It should be at least 8 characters long, contain uppercase letters, lowercase letters, at least 2 digits, and no spaces"
+    if (user.length != 0) {
+      res.render("contactlog");
+    }
+    if (!passwordValidation) {
+      res.render("contact");
+    } 
+      else {
+      const newuser = await contactModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.render("contactlog");
+    }
+  } catch (error) {}
+});
+router.post("/login", async (req, res) => {
+  try {
+    let user = await contactModel.find({ email: req.body.email, password:req.body.password});
+    console.log(user);
+    
+    if (user.length != 0) {
+      res.render("index");
+    } else {
+      res.render("contact");
+    }
+  } catch (error) {}
 });
 
-// Contact Form data Render
-router.get("/show-request", async (req, res) => {
+
+
+
+router.get("/showdata", async (req, res) => {
   let data = await contactModel.find();
   res.send(data);
 });
+
+
+router.get("/delete", async (req, res) => {
+  let data = await contactModel.deleteMany();
+  res.send(data);
+});
+
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/school");
 
 // Define schema of students
 
-// let studentSchema = mongoose.Schema({
-//   name: String,
-//   age: Number,
-//   grade: String,
-//   address: String,
-// });
-
 let contactSchema = mongoose.Schema({
   name: String,
   email: String,
-  message: String,
-  contact: Number,
+  password: String,
 });
 
 
-// module.exports = mongoose.model("Student",studentSchema);
 module.exports = mongoose.model("contact",contactSchema);
